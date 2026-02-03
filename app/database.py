1e7c038efe95407d3840b8fca4cd0fd79fbffc23
DungDaot08 <dungdaot08@gmail.com>
2026-02-03 10:41:50 +0700

    Update

--- app/database.py
+++ app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "postgresql://postgres:123@localhost:5434/task_schedule"
-# DATABASE_URL = "postgresql://task_schedule_user:HSMfkigbqhS1SRY1YelIWVBwr5O0vZYV@dpg-d60lsenfte5s73b88430-a/task_schedule"
-DATABASE_URL = "postgresql://task_schedule_user:HSMfkigbqhS1SRY1YelIWVBwr5O0vZYV@dpg-d60lsenfte5s73b88430-a.oregon-postgres.render.com/task_schedule"
+DATABASE_URL = "postgresql://task_schedule_user:HSMfkigbqhS1SRY1YelIWVBwr5O0vZYV@dpg-d60lsenfte5s73b88430-a/task_schedule"
+# DATABASE_URL = "postgresql://task_schedule_user:HSMfkigbqhS1SRY1YelIWVBwr5O0vZYV@dpg-d60lsenfte5s73b88430-a.oregon-postgres.render.com/task_schedule"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
