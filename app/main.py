5085004f9d70bec5020d4441cf4c018bb57c6233
DungDaot08 <dungdaot08@gmail.com>
2026-03-17 10:30:08 +0700

    Update

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.message import router as message_router
 from app.api.user_api import router as user_router
 from app.api.task_api import router as task_router
@@ -6,6 +7,19 @@ from app.api.auth import router as auth_router
 from app.api.ws import websocket_endpoint
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://your-frontend-domain.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(message_router)
 app.include_router(user_router)
